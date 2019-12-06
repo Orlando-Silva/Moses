6f57f40984206eda5cea742088400ad06f777421
orlandosilva <orlando.silva@contabilizei.com.br>
2019-12-06 18:05:30 -0300

    Fixes

--- src/partitionMaker.js
+++ src/partitionMaker.js
@@ -7,21 +7,46 @@ module.exports = {
      
         let partitions = [];
 
-        while(hasEntries(ofxFile)) {
-            let partition = generateNewPartitionFromFile(ofxFile, maxEntries);
+        console.log('OFX tem ' + ofxFile.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN.length + ' lançamentos.');
 
+        ofxFile.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN = generateUniqueDescription(ofxFile);
+
+        while(hasEntries(ofxFile)) {
+                  
+            let partition = generateNewPartitionFromFile(ofxFile, maxEntries);
+        
             // Para evitar que ambos objetos apontem para o mesmo endereço de memória.
             let ofxStructureShallowCopy = JSON.parse(JSON.stringify(ofxFile));
 
             // Para pegar a estrutura do OFX mas manter apenas os lançamentos que desejamos.
             ofxStructureShallowCopy.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN = partition;
 
-            let partitionEntries = extractEntries(ofxFile, ofxStructureShallowCopy.body);
+            let partitionEntries = extractEntries(ofxFile, ofxStructureShallowCopy.body.OFX);
 
             partitions.push(partitionEntries);            
         }
         return partitions;
+        
     },
+}
+
+function generateUniqueDescription(ofxFile) {
+    ofxFile.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN.forEach(element => {
+        if(!isUnique(ofxFile, element)) {
+            element.MEMO = element.MEMO + ' ' + generateUniqueId(ofxFile, element);
+        }
+    });
+    return ofxFile.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN;
+}
+
+function generateUniqueId(ofxFile, currentEntry) {
+    let ofxStructureShallowCopy = JSON.parse(JSON.stringify(ofxFile));
+    return ofxStructureShallowCopy.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN.filter(f => { return f.MEMO == currentEntry.MEMO && f.TRNAMT == currentEntry.TRNAMT && f.DTPOSTED == currentEntry.DTPOSTED }).length;
+}
+
+function isUnique(ofxFile, currentEntry) {
+    let ofxStructureShallowCopy = JSON.parse(JSON.stringify(ofxFile));
+    return ofxStructureShallowCopy.body.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN.filter(f => { return f.MEMO == currentEntry.MEMO && f.TRNAMT == currentEntry.TRNAMT && f.DTPOSTED == currentEntry.DTPOSTED }).length == 1;
 }
 
 function extractEntries(ofxFile, partition) {
